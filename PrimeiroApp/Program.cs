2cd70ca549060f45428489f6a48b23e7658806ed
Nilo Sandes <Nilo Sandes@DESKTOP-R3MH7RQ>
2021-09-16 15:35:37 -0300

    Adicionar arquivos de projeto.

---
+++ PrimeiroApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PrimeiroApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá, Nilo Sandes!");
+        }
+    }
+}
