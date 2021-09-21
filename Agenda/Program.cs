a382000957e61ea2cdedeb4851223151e0c8eb5a
Nilo Sandes <Nilo Sandes@DESKTOP-R3MH7RQ>
2021-09-21 14:52:06 -0300

    Start the coding of a test of a Schedule Project in C#

---
+++ Agenda/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Agenda
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            String nome;
+            String sobrenome;
+            String compNome = "$nome $sobrenome"; //deveria ser um objeto
+            //matriz telefone com var DDD e num?
+            String email;
+        }
+    }
+}
